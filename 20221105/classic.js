d46e4fe00e2b5c0d30987124569f4613fe109eaf
loveyrooney <loveyrooney@gmail.com>
2022-11-12 17:26:14 +0900

    feat: 8th commit

--- 20221105/classic.js
+++ 20221105/classic.js
@@ -1,27 +1,33 @@
-function modal(){
-    $(".hidden").removeClass(".hidden");
-} // 부트스트랩 모달창은 토글적용되어있음. 이거 내가 만든 서브밋 버튼기능이랑 충돌하는거 해결해야됨.
+const modalback = document.querySelector("#modalback");
+const modalbody = document.querySelector("#modalbody");
+const closebtn = document.querySelector("#close");
 
 function submit(){
+    modalback.classList.remove("hidden");
+    modalbody.classList.remove("hidden");
     var checkedRadio = $("input[name=flexRadioDefault]:checked");
     if(checkedRadio.val() == "on"){
-        $("#submitUl").append(`<li>${$(`label[for=${$(checkedRadio)[0].id}]`).text()}</li>`);
+        $("#submitUl").append(`<li style="list-style:none;">${$(`label[for=${$(checkedRadio)[0].id}]`).text()}</li>`);
     }   
     var checkedAge = $("input[type=checkbox]:checked");
     if(checkedAge[0].checked == true){
-        $("#submitUl").append(`<li>${$(`label[for=${$(checkedAge)[0].id}]`).text()}</li>`);
+        $("#submitUl").append(`<li style="list-style:none;">${$(`label[for=${$(checkedAge)[0].id}]`).text()}</li>`);
     }
     var composer = $("#composer");
     var work = $("#work");
     if(composer[0].value !== ''){
-        $("#submitUl").append(`<li>${composer[0].value}</li>`);
+        $("#submitUl").append(`<li style="list-style:none;">${composer[0].value}</li>`);
     } 
     if(work[0].value !== ''){
-        $("#submitUl").append(`<li>${work[0].value}</li>`);
+        $("#submitUl").append(`<li style="list-style:none;">${work[0].value}</li>`);
+    } else {
+        $("#submitUl").append(`<li style="list-style:none;">Please write on your tastes</li>`);
     }
-    modal();
 }
 
 function close(){
-    $(".hidden").addClass(".hidden");
+    modalback.classList.add("hidden");
+    modalbody.classList.add("hidden");
 }
+closebtn.addEventListener("click", close);
+
